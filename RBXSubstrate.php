b5e1d8e3a26746ecf47c8825e58b6b0a9c840ebc
Ian Murphy <immur.50ae@gmail.com>
2016-05-12 22:41:17 -0700

    Added several helper methods for file access and xml writing.

---
+++ RBXSubstrate.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ian
+ * Date: 5/12/16
+ * Time: 7:35 PM
+ */
+class RBXSubstrate
+{
+    protected $_currentDirectory;
+    protected $_rootDirectory;
+    /*
+     * Parameters: (simpleXML object, path to save location
+     */
+    public static function XML2File(SimpleXMLElement $xmlObject, string $path)
+    {
+        $fileHandle = fopen($path, "w") or die("Could not open file " . $path . " for writing.");
+        $xmlString = $xmlObject->asXML();
+        fwrite($fileHandle, $xmlString, strlen($xmlString));
+    }
+
+    public static function File2XML(string $path)
+    {
+        $fileHandle = fopen($path, "r") or die("Could not open file " . $path . " for reading.");
+        $xmlString = fread($fileHandle, filesize($path));
+        return simplexml_load_string($xmlString);
+    }
+
+    public function __construct(string $rootDirectory)
+    {
+        $this->_rootDirectory = $rootDirectory;
+    }
+
+    public function CreateDirectory(string $path, bool $changeDir = true)
+    {
+        if(mkdir($path, 0777, true))
+        {
+            if($changeDir) $this->_currentDirectory = $path;
+            return true;
+        }
+        return false;
+    }
+
+    public function CurrentDirectory()
+    {
+        return $this->_currentDirectory;
+    }
+
+    public function MoveUpLevel()
+    {
+        if($this->_currentDirectory != null)
+        {
+            $this->_currentDirectory = dirname($this->_currentDirectory);
+            return $this->_currentDirectory;
+        }
+        return false;
+    }
+
+    public function SetCurrentDirectory(string $path)
+    {
+        if(file_exists($path))
+        {
+            $this->_currentDirectory = $path;
+            return true;
+        }
+        return false;
+    }
+
+    public function GetRootDirectory()
+    {
+        return $this->_rootDirectory;
+    }
+}
