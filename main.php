f577eb5f04b6c088da2746dc3253d045c3a3c070
Ian Murphy <immur.50ae@gmail.com>
2016-05-14 13:40:17 -0700

    Changed project layout.  Directory creation sort of works.

--- main.php
+++ main.php
@@ -6,12 +6,14 @@
  * Time: 4:05 PM
  */
 
-include_once("RBXDirectory.php");
+include_once("src/RBXDirectory.php");
+include_once("src/RBXSubstrateDummy.php");
 //var_dump($argv);
 $filename = $argv[1];
 echo $filename . "\n";
 $xml = RBXSubstrate::File2XML($filename);
 //var_dump($xml);
+$rbxDummy = new RBXSubstrateDummy(dirname(__FILE__));
 $parser = new RBXDirectory(dirname(__FILE__));
 $parser->generateDirFromXml($xml);
 //var_dump($parser);
